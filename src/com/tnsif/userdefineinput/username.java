cf767344b81d81e56ef9625fac3ef27fe4c59146
shaikadhil79-hash <shaikadhil79@gmail.com>
2025-12-24 09:49:51 +0530

    interface

---
+++ src/com/tnsif/userdefineinput/username.java
@@ -0,0 +1,13 @@
+package com.tnsif.userdefineinput;
+import java.util.Scanner;
+
+public class username {
+	public static void main(String[] args) {
+		Scanner input  = new Scanner(System.in);
+		System.out.println("Enter the name:");
+		String n=input.nextLine();
+		System.out.println("Hello"+n);
+		
+	}
+
+}
